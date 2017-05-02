f45de341a639e7c7d6f76a7ea42833b22fbb6d16
Dmitriy <vashishin@itstep.org>
2017-05-02 12:08:02 +0300

    add new files

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
+var gulp = require('gulp'); // главная переменная пректа. Инициальзация Gulp
+var sass = require('gulp-sass'); //Подключаем Sass пакет
 
+gulp.task('myfirstask', function() {
+    // я просто комментарий
+    var name = "Dmitriy";
+    console.log("Привет меня зовут "+ name);
+    console.log('Это Ваша первая инструкция (Task)');
+});
+
+
+
+gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('app/sass/style.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством пакета gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+});
